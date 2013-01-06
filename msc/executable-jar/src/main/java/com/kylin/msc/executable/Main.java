9a9168db5d24470c2365067fd3b2edfd8775763e
Kylin <songzhiqi_1214@yahoo.com.cn>
2013-01-06 18:14:47 +0800

    add modular service container code

---
+++ msc/executable-jar/src/main/java/com/kylin/msc/executable/Main.java
@@ -0,0 +1,12 @@
+package com.kylin.msc.executable;
+
+public class Main {
+	
+	private final static String VERSION = "1.0.0";
+
+	public static void main(String[] args) {
+		
+		System.out.printf("\n    Modular Service Container run executable jar archive %s\n\n", VERSION);
+	}
+
+}
