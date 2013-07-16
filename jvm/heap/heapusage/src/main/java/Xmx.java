0471aa6854b05c5013fefabc2fc89ced74f43fc2
Kylin <kylinsoong.1214@gmail.com>
2013-07-16 22:07:14 +0800

    update heapuaage code

--- jvm/heap/heapusage/src/main/java/Xmx.java
+++ jvm/heap/heapusage/src/main/java/Xmx.java
@@ -6,14 +6,23 @@ public class Xmx {
 
 	public static void main(String[] args) {
 		
-		long defValue = System.currentTimeMillis();
+		new Thread(new Runnable(){
 
-		Map<String, Object> map = new HashMap<String, Object>();
+			public void run() {
+				Thread.currentThread().setName("heap-swallow-thread");
+				
+				long defValue = System.currentTimeMillis();
+
+				Map<String, Object> map = new HashMap<String, Object>();
+				
+				for(int i = 0 ; ; i++) {
+					System.out.println(i + " - " + (System.currentTimeMillis() - defValue) + ", maxMemory: " + Runtime.getRuntime().maxMemory() + ", totalMemory: " + Runtime.getRuntime().totalMemory() + ", freeMemory: " + Runtime.getRuntime().freeMemory());
+					map.put("key-" + i, new byte[1024 * 1000 ]);
+				}
+			}
+		}).start();
 		
-		for(int i = 0 ; ; i++) {
-			System.out.println(i + " - " + (System.currentTimeMillis() - defValue) + ", maxMemory: " + Runtime.getRuntime().maxMemory() + ", totalMemory: " + Runtime.getRuntime().totalMemory() + ", freeMemory: " + Runtime.getRuntime().freeMemory());
-			map.put("key-" + i, new byte[1024 * 1000 ]);
-		}
+		
 	}
 
 }
